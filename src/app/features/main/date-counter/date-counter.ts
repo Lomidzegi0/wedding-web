ba4918cc08bbf45588fdc7227515b0bd1facd7a4
mikheili-silagava-tbc-it-academy <mikheili.silagava.1@btu.edu.ge>
2026-07-03 00:51:09 +0400

    feature(display): display base, hero

---
+++ src/app/features/main/date-counter/date-counter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-counter',
+  imports: [],
+  templateUrl: './date-counter.html',
+  styleUrl: './date-counter.scss',
+})
+export class DateCounter {
+
+}
